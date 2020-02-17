1f8901465da1a6d9adecdc53c5f7ba0335063db3
Kate Ward <kate.ward@forestent.com>
2020-02-17 13:17:39 +0100

    Moved options into their own file. Added some global flag values.

--- carbonio/devices/devices.go
+++ carbonio/devices/devices.go
@@ -4,8 +4,6 @@ Package devices enables control of specific Avid S3L devices.
 package devices
 
 import (
-	"net"
-
 	"github.com/kward/avid-s3l/carbonio/signals"
 )
 
@@ -17,44 +15,3 @@ type Device interface {
 }
 
 const SPIDevicesDir = "/sys/bus/spi/devices"
-
-type options struct {
-	mac  net.HardwareAddr
-	ip   net.IP
-	host string
-
-	spiBaseDir string
-}
-
-func Host(v string) func(*options) error {
-	return func(o *options) error { return o.setHost(v) }
-}
-func (o *options) setHost(v string) error {
-	o.host = v
-	return nil
-}
-
-func IP(v net.IP) func(*options) error {
-	return func(o *options) error { return o.setIP(v) }
-}
-func (o *options) setIP(v net.IP) error {
-	o.ip = v
-	return nil
-}
-
-func MAC(v net.HardwareAddr) func(*options) error {
-	return func(o *options) error { return o.setMAC(v) }
-}
-func (o *options) setMAC(v net.HardwareAddr) error {
-	o.mac = v
-	return nil
-}
-
-// SPIBaseDir returns the path to the SPI devices directory.
-func SPIBaseDir(v string) func(*options) error {
-	return func(o *options) error { return o.setSPIBaseDir(v) }
-}
-func (o *options) setSPIBaseDir(v string) error {
-	o.spiBaseDir = v
-	return nil
-}
---
+++ carbonio/devices/devices_opts.go
@@ -0,0 +1,56 @@
+package devices
+
+import "net"
+
+type options struct {
+	// Device options.
+	mac  net.HardwareAddr
+	ip   net.IP
+	host string
+
+	// Global flags.
+	spiBaseDir string
+	verbose    bool
+}
+
+func Host(v string) func(*options) error {
+	return func(o *options) error { return o.setHost(v) }
+}
+func (o *options) setHost(v string) error {
+	o.host = v
+	return nil
+}
+
+func IP(v net.IP) func(*options) error {
+	return func(o *options) error { return o.setIP(v) }
+}
+func (o *options) setIP(v net.IP) error {
+	o.ip = v
+	return nil
+}
+
+func MAC(v net.HardwareAddr) func(*options) error {
+	return func(o *options) error { return o.setMAC(v) }
+}
+func (o *options) setMAC(v net.HardwareAddr) error {
+	o.mac = v
+	return nil
+}
+
+// SPIBaseDir returns the path to the SPI devices directory.
+func SPIBaseDir(v string) func(*options) error {
+	return func(o *options) error { return o.setSPIBaseDir(v) }
+}
+func (o *options) setSPIBaseDir(v string) error {
+	o.spiBaseDir = v
+	return nil
+}
+
+// Verbose returns the verbose output setting.
+func Verbose(v bool) func(*options) error {
+	return func(o *options) error { return o.setVerbose(v) }
+}
+func (o *options) setVerbose(v bool) error {
+	o.verbose = v
+	return nil
+}
